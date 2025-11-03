f352ff362f7de26334ae5af1963dc22a035b21f7
rekarozsar <rekarozsar0012@gmail.com>
2025-11-03 16:07:01 +0100

    Removed login from wrong folder

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
 
 export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/welcome' },
   { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) }
+  { path: 'login', pathMatch: 'full', component: Login}
 ];
