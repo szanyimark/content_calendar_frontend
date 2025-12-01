60b6c35df1e475c1d0244b5b20217f9fe8fa462a
szanyimark <szanyimark04@gmail.com>
2025-12-01 17:08:09 +0100

    interceptor fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,7 +6,7 @@ import { uk_UA, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import uk from '@angular/common/locales/uk';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors, withXsrfConfiguration } from '@angular/common/http';
 import { xsrfInterceptor } from './interceptors/xsrf.interceptor';
 
 registerLocaleData(uk);
@@ -18,6 +18,11 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideNzI18n(uk_UA),
     provideAnimationsAsync(),
-    provideHttpClient(withInterceptors([xsrfInterceptor]))
+    // Disable Angular's default XSRF cookie-based header by pointing at a non-existent cookie,
+    // so our interceptor-provided header is not overwritten.
+    provideHttpClient(
+      withXsrfConfiguration({ cookieName: 'XSRF-TOKEN-NONE', headerName: 'X-XSRF-TOKEN' }),
+      withInterceptors([xsrfInterceptor])
+    )
   ]
 };
