4157ada7607fedcc774b84a630f08f8d4c38c756
szanyimark <szanyimark04@gmail.com>
2025-12-01 17:01:25 +0100

    bootstrap interceptor

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { provideNzI18n, hu_HU } from 'ng-zorro-antd/i18n';
-import { NZ_DATE_CONFIG } from 'ng-zorro-antd/i18n';
-import { provideRouter } from '@angular/router';
-import { routes } from './app/app.routes';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
-import { importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 
-bootstrapApplication(App, {
-  providers: [
-    provideZoneChangeDetection(),provideRouter(routes),
-    provideAnimations(),
-    BrowserAnimationsModule,
-    provideHttpClient(),
-    provideNzI18n(hu_HU),
-    {
-      provide: NZ_DATE_CONFIG,
-      useValue: {
-        firstDayOfWeek: 1  // 1 = Monday
-      }
-    }
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(App, appConfig).catch(err => console.error(err));
