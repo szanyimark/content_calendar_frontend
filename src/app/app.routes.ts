f19547ad62fb8b3de6225ad51d4159841b05ca93
szanyimark <szanyimark04@gmail.com>
2025-12-02 13:34:17 +0100

    root redirect to main

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Login } from './pages/login/login';
 import { MainLayoutComponent } from './components/main-layout/main-layout';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '', redirectTo: 'main', pathMatch: 'full' },
   { path: 'login', pathMatch: 'full', component: Login},
   { path: 'main', component: MainLayoutComponent }
 ];
